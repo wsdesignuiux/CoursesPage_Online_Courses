1b5e52a6a7f499a46f1b4eb43871bfb39033ba78
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-17 11:59:29 +0530

    Initial commit

---
+++ app/src/main/java/e/wolfsoft1/coursespage_online_courses/Courses_Page_OnlineCoursesActivity.java
@@ -0,0 +1,13 @@
+package e.wolfsoft1.coursespage_online_courses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Courses_Page_OnlineCoursesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses__page__online_courses);
+    }
+}
